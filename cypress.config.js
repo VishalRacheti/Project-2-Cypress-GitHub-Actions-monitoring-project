4d49c24fe99e6ab5c563e9f8ff0b26bce7613781
root <root@Vishal.localdomain>
2025-10-19 21:11:04 +0530

    project

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here if needed
     },
-    baseUrl: 'https://example.co',
+    baseUrl: 'https://example.com',
     specPattern: 'cypress/e2e/**/*.js',
     supportFile: 'cypress/support/e2e.js',
   },
